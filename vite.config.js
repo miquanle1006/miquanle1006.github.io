71a7b8a4dac1d0c47201cc6c7b14f50312529fb3
QuanLew <quan,le01@sjsu.edu>
2025-09-20 14:28:04 -0700

    first commit

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,8 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ["**/*.glb"],
-  base: "./",
+  base: "/", // root domain deployment, for example: https://quanle1006.github.io/
+//   base: '/YOUR_REPOSITORY_NAME/' --- IGNORE --- for example: https://quanle1006.github.io/quan-portfolio/
+// cause we are using user/organization site, so the base should be '/'
+// if we are using project site, the base should be '/YOUR_REPOSITORY_NAME/'
 });
